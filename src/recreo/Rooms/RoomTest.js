9463c76eb61c15907e9d7d869b79b9f3f4177c60
Cesar-CARV <cesaruizvera@gmail.com>
2024-12-26 15:28:51 -0600

    feat: Se agreglo la clase Linea

--- src/recreo/Rooms/RoomTest.js
+++ src/recreo/Rooms/RoomTest.js
@@ -1,17 +1,26 @@
 import { Room } from "recreo";
 import Shape from "../Objects/Shape";
 import SelectTool from "../Tools/SelectTool/SelectTool";
+import Line from "../Objects/Line";
 
 class RoomTest extends Room {
   constructor(GAME) {
     super(GAME);
 
     const shape = new Shape(this._GAME, 50, 50, 50, 100);
-    // shape.setType("CIRCLE");
     shape.setBorderType("DASHED");
     shape.setRadius(80, 0, 0, 0);
+    
+    const shape2 = new Shape(this._GAME, 300, 50, 50, 50);
+    shape2.setType("CIRCLE");
+    shape2.setBorderType("LINE");
+    shape2.borderWidth = 5;
 
+    const line = new Line(this._GAME, 200, 300, 50, 50);
+    
     this.addInstance(shape, false, "shape01");
+    this.addInstance(shape2, false, "shape02");
+    this.addInstance(line, false, "line01");
     this.addInstance(new SelectTool(GAME), false, "select-tool");
   }
 }
