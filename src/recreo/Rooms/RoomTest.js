5883cca3f8045b9fa7adfe7be86d511433c65b6b
Cesar-CARV <cesaruizvera@gmail.com>
2025-01-10 10:31:42 -0600

    refactor: Se eliminaron las shapes por defecto y se cambiaron los colores por defecto

--- src/recreo/Rooms/RoomTest.js
+++ src/recreo/Rooms/RoomTest.js
@@ -10,15 +10,6 @@ class RoomTest extends Room {
   constructor(GAME) {
     super(GAME);
 
-    const shape = new Shape(this._GAME, 50, 50, 50, 100);
-    shape.setBorderType("DASHED");
-    shape.setRadius(80, 0, 0, 0);
-
-    const shape2 = new Shape(this._GAME, 300, 50, 50, 50);
-    shape2.setType("CIRCLE");
-    shape2.setBorderType("LINE");
-    shape2.borderWidth = 5;
-
     // Tool controller
     const toolController = new ObjectNode(GAME, 0, 0, 0, 0);
     toolController.steps = () => {
@@ -75,8 +66,6 @@ class RoomTest extends Room {
       }
     };
 
-    this.addInstance(shape, false, "shape01");
-    this.addInstance(shape2, false, "shape02");
     this.addInstance(toolController, false, "tool-controller");
     this.addInstance(new SelectTool(GAME), false, "select-tool");
     this.addInstance(new PenTool(GAME), false, "pen-tool");
