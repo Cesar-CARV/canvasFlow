7d6d6e46e4aa6901d5f94f42e722940b64e565a9
Cesar-CARV <cesaruizvera@gmail.com>
2025-01-25 12:54:58 -0600

    fix: Se corrigo el bug que causaba que se encogieran los iconos

--- src/icons/IconBase.jsx
+++ src/icons/IconBase.jsx
@@ -6,6 +6,7 @@ export default function IconBase({ children, width, height, lineWidth }) {
       xmlns="http://www.w3.org/2000/svg"
       width={width ? width : 24}
       height={height ? height : 24}
+      style={{minWidth: width}}
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
